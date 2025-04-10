f6e9db5127eb049603de911e815a8cfb3c76a5b9
Ronnie Alarcon <ronnieha35@gmail.com>
2025-04-09 21:36:37 -0500

    Reto

---
+++ RepasoModulo1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RepasoModulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, filas;
+
+            Console.WriteLine("Ingresa el numero de Filas");
+            filas = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= filas; i++)
+            {
+                Console.Write("* ");
+
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("Se acabo el Programa");
+        }
+    }
+}
